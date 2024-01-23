db29a21059cc6e7324f40b18551b3be5e65a6a71
josé carlos <josecampos2030.jdk@gmail.com>
2024-01-23 13:13:25 -0300

    update

--- Run and gun/Assets/Scripts/Player.cs
+++ Run and gun/Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     private bool onGround = false;
     private Transform groundCheck;
     private float hForce = 0f;
+    [SerializeField] private Animator myAnim;
 
     private bool isDead = false;
 
@@ -22,13 +23,14 @@ public class Player : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         //encontrando o transform do objeto filho groundcheck
         groundCheck = gameObject.transform.Find("GroundCheck");
+        myAnim = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (!isDead)
         {
-            //criando uma linha que vai do player até o GroundCheck, na layer ground
+            //criando uma linha que vai do player atï¿½ o GroundCheck, na layer ground
             onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
             if (Input.GetButtonDown("Jump") && onGround)
             {
@@ -37,7 +39,7 @@ public class Player : MonoBehaviour
         }
         else if (Input.GetButtonUp("Jump"))
         {
-            //fazendo que quando eu solte a barra de espaço, o pulo perca a intensidade
+            //fazendo que quando eu solte a barra de espaï¿½o, o pulo perca a intensidade
             if(rb2d.velocity.y > 0)
             {
                 rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
@@ -49,8 +51,13 @@ public class Player : MonoBehaviour
     {
         if (!isDead)
         {
+            if(rb2d.velocity.x == 0)
+            {
+                myAnim.SetBool("Run", false);
+            }
             hForce = Input.GetAxisRaw("Horizontal");
             rb2d.velocity = new Vector2(hForce * speed, rb2d.velocity.y);
+            myAnim.SetBool("Run", true);
             if(hForce > 0 && !facinRight)
             {
                 Flip();
@@ -62,7 +69,7 @@ public class Player : MonoBehaviour
             if (jump)
             {
                 jump = false;
-                //adicionando uma força no ridbody2d
+                //adicionando uma forï¿½a no ridbody2d
                 rb2d.AddForce(Vector2.up * jumpForce);
             }
         }
@@ -74,5 +81,6 @@ public class Player : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+        
     }
 }
