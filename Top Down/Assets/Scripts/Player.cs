29fcf1cb24c803c86620100102829e08f6855a7a
josé carlos <carlinho2025@gmail.com>
2024-06-24 22:52:22 -0300

    update

--- Top Down/Assets/Scripts/Player.cs
+++ Top Down/Assets/Scripts/Player.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 15f;
+    [SerializeField] private Rigidbody2D myRB;
+    private Animator playerAnimator;
+    private float horizontal;
+    private float vertical;
+    private Vector2 move;
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        this.horizontal = Input.GetAxis("Horizontal");
+        this.vertical = Input.GetAxis("Vertical");
+        // uma forma de se fazer o personagem andar
+        /* transform.position += new Vector3(horizontal, vertical, 0f) * speed * Time.deltaTime;*/  
+    }
+
+    void FixedUpdate()
+    {
+        move = new Vector2 (horizontal, vertical);
+        if(move.sqrMagnitude  > 0)
+        {
+            playerAnimator.SetInteger("Movimento", 1);
+        }
+        else
+        {
+            playerAnimator.SetInteger("Movimento", 0);
+        }
+        /*   move.Normalize();*/
+        //mais outra forma de se fazer o personagem andar
+        /*myRB.velocity = move * speed;*/
+        // a forma a seguir foi a utilizada pelo professor
+        //fixedDeltaTime -> valor fixo
+        myRB.MovePosition(myRB.position + move * speed * Time.fixedDeltaTime);
+        Flip();
+    }
+
+    void Flip()
+    {
+        if (move.x > 0) 
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+        if(move.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
     }
 }
