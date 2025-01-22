7717ccd295a3349c8d030a5dc1b33fb81a111e71
josé carlos <carlinho2025@gmail.com>
2025-01-21 22:32:34 -0300

    update

---
+++ jogo de laura/Assets/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
